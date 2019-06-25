48fee17b05dd841d956ab8e5a021956e70bf4deb
Cuong Gosu <cuonggosu@Cuongs-iMac.local>
2019-06-25 15:54:39 +0700

    react-native-text-input-mask

--- android/app/src/main/java/com/chatgroupfirebase/MainApplication.java
+++ android/app/src/main/java/com/chatgroupfirebase/MainApplication.java
@@ -3,6 +3,7 @@ package com.chatgroupfirebase;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.RNTextInputMask.RNTextInputMaskPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNTextInputMaskPackage(),
             new RNFirebasePackage(),
             new AsyncStoragePackage(),
             new RNGestureHandlerPackage(),
